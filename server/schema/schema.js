122feb6da53c7f4b46be2ada41f1cd41b86dd44e
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2023-12-12 18:07:37 +0530

    schemajs commit

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,4 @@
-import { GraphQLID, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
+import { GraphQLEnumType, GraphQLID, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 
 import ProjectSchema from "../models/Project.js";
 import ClientSchema from "../models/Client.js";
@@ -38,41 +38,106 @@ const ProjectType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args) {
                 return ProjectSchema.find();
             }
         },
-
         project: {
             type: ProjectType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return ProjectSchema.findById(args.id);
             }
+        },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return ClientSchema.find();
+            }
+        },
+        client: {
+            type: ClientType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return ClientSchema.findById(args.id);
+            }
         }
     },
+});
 
-    clients: {
-        type: new GraphQLList(ClientType),
-        resolve(parent, args) {
-            return ClientSchema.find();
-        }
+
+// Mutations
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        // Add a client from the mongoose db (visible on compass)
+        addClient: {
+            type: ClientType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                email: { type: GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parent, args) {
+                const client = new ClientSchema({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone,
+                });
+
+                return client.save();
+            }
+        },
+        // Delete a Client from the mongoose db (visible on compass)
+        deleteClient: {
+            type: ClientType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args) {
+                return ClientSchema.findOneAndDelete({ _id: args.id })
+            },
+        },
+        // Add a project
+        addProject: {
+            type: ProjectType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                description: { type: GraphQLNonNull(GraphQLString) },
+                status: {
+                    type: new GraphQLEnumType({
+                        name: "ProjectStatus",
+                        values: {
+                            'new': { value: 'Not Started' },
+                            'progress': { value: 'In Progress' },
+                            'completed': { value: 'Completed' },
+                        }
+                    }),
+                    defaultValue: 'Not Started',
+                },
+                clientId: { type: GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args) {
+                const project = new ProjectSchema({
+                    name: args.name,
+                    description: args.description,
+                    status: args.status,
+                    clientId: args.clientId,
+                });
+
+                return project.save();
+            },
+        },
     },
-
-    client: {
-        type: ClientType,
-        args: { id: { type: GraphQLID } },
-        resolve(parent, args) {
-            return ClientSchema.findById(args.id);
-        }
-    }
 });
 
 const schema = new GraphQLSchema({
     query: RootQuery,
+    mutation: mutation
 });
 
 export default schema;
+
+// 1:08:55 he got the error bcz of clients, we got it right. no worry, start it from here.
